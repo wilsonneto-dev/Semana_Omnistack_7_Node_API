d2e937c09a294c548189f1bff91aab2e69d945ba
Wilson Gomes <contato@wilsonneto.com.br>
2019-11-17 11:39:55 -0300

    node env validations

--- src/configs/dotenv.js
+++ src/configs/dotenv.js
@@ -2,7 +2,15 @@ const dotenv = require('dotenv');
 const fs = require('fs');
 const path = require('path');
 
-if (fs.existsSync(path.resolve(__dirname, '..', '..', '.env'))) {
+console.log('env: ', process.env.NODE_ENV);
+console.log('name: ', process.env.NAME);
+
+const env = process.env.NODE_ENV || 'dev';
+
+if (
+  env == 'dev' &&
+  fs.existsSync(path.resolve(__dirname, '..', '..', '.env'))
+) {
   dotenv.config({ path: '.env' });
 }
 
